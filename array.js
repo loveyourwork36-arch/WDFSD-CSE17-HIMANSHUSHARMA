bbe7b57af82d8699c3e9595b31f60f4f49ff4960
HimanshuSharma <loveyourwork36@gmail.com>
2026-06-24 03:23:46 +0530

    array

---
+++ array.js
@@ -0,0 +1,11 @@
+const dc =["spiderman","batman"];
+const marvel =["ironman","captain america"];
+ const new_array=dc.concat(marvel);
+ console.log(new_array);
+ console.log(...dc,...marvel); // spread operator  used to merege arrays 
+ console.log(Array.from("hello"));// convert into array
+ console.log(Array.of(1,2,3,4,5)); // convert into array 
+ const arr=[1,2,3,4,5];
+ console.log(arr.join());
+
+
