0818851f9a1cdb0269e40b76998e058610574cb8
HimanshuSharma <loveyourwork36@gmail.com>
2026-06-24 01:25:43 +0530

    Initial commit

---
+++ basic.js
@@ -0,0 +1,18 @@
+console.log("him")
+// data type conversion 
+let  score ="33"
+let valuein= Number(score)
+console.log(typeof score)
+console.log(typeof valuein)
+/* "33" = 33
+    "33abc"= Nan
+    "him " = Nan*/
+    let  isloggedin=1
+    let a =Boolean(isloggedin)
+    console.log(a)
+    console.log(undefined >0)
+    console.log(undefined ==0)
+    console.log(undefined <=0)
+    let meow =[1,2,3]
+    console.log(meow)
+
---
+++ string.js
@@ -0,0 +1,5 @@
+let name ="him"
+ const  gamename= new String("meowwww")
+console.log(gamename.toUpperCase())
+console.log(gamename.substring(0,4))
+console.log(gamename.slice(-6,3))
